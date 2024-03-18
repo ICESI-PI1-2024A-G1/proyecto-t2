ce6e12944ddf66991da5a2b25ed3feebfbc37307
Juan Felipe Jojoa Crespo <juanfejojoa@gmail.com>
2024-03-18 09:33:57 -0500

    JFJC/feat: Added Form of Register Schedule

--- code/CCSApp/forms.py
+++ code/CCSApp/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Horario
 
-class HorarioForm(forms.ModelForm):
-    class Meta:
-        model = Horario
-        fields = ['fecha_hora', 'profesor', 'materia', 'modalidad', 'enlace_virtual', 'salon_presencial']
+class NewHorario(forms.Form):
+    fecha_hora = forms.DateTimeField(label="Fecha y Hora", widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+    profesor = forms.CharField(label="Profesor", max_length=100)
+    materia = forms.CharField(label="Materia", max_length=255)
+    MODALIDAD_CHOICES = [
+        ('presencial', 'Presencial'),
+        ('virtual', 'Virtual'),
+        ('mixta', 'Mixta'),
+    ]
+    modalidad = forms.ChoiceField(label="Modalidad", choices=MODALIDAD_CHOICES)
+    enlace_virtual = forms.URLField(label="Enlace Virtual", required=False)
+    salon_presencial = forms.CharField(label="Salon Presencial", max_length=50, required=False)
