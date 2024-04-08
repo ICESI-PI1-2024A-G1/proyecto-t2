81601b53aa94c460471e6b0825ff4c95c3103aeb
Juan Sebastian Caviedez Fernandez <1005945503@u.icesi.edu.co>
2024-04-07 22:40:57 -0500

    JSCF/feat: Modifications
    Updated HTML templates and added JavaScript for page transitions
    
    The changes include:
    
    * Adding a favicon to all HTML templates
    * Updating the page container and page transitions in the servicios\_asignacion.html template
    * Adding JavaScript functions for page transitions in transiciones.js
    * Adding functions for redirection and page transitions in transiciones.js
    
    The new functions in transiciones.js are:
    
    * redirigirServiciosAsignaciondesdeAsignarHorario()
    * redirigirAsignarHorario()
    * redirigirConsultarHorario()
    * redirigirServiciosAsignaciondesdeConsultarHorarios()
    * redirigirModificarHorario()
    * redirigirCrearEspacio()
    
    These functions are used for redirection and page transitions between the servicios\_asignacion, asignar\_horario, and consultar\_horarios pages.

--- code/CCSApp/templates/static/js/transiciones.js
+++ code/CCSApp/templates/static/js/transiciones.js
@@ -204,3 +204,141 @@ function redirigirServiciosAsignaciondesdeRegistro() {
         xhttp.send();
     }, 500);
 }
+
+function redirigirServiciosAsignaciondesdeAsignarHorario() {
+    document.getElementById("page1").classList.add("fade-out");
+    
+    setTimeout(function() {
+        // Ocultar la página 1
+        document.getElementById("page1").style.display = "none";
+        
+        // Cargar la página 2 usando AJAX
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Insertar el contenido de la página 2 en el DOM
+                document.getElementById("page-container").innerHTML = this.responseText;
+                
+                // Aplicar la animación de entrada a la página 2
+                document.getElementById(window.location.href = '/index/servicios_asignacion').classList.add("fade-in");
+            }
+        };
+        xhttp.open("GET", window.location.href = '/index/servicios_asignacion', true);
+        xhttp.send();
+    }, 500);
+}
+
+function redirigirAsignarHorario() {
+    document.getElementById("page1").classList.add("fade-out");
+    
+    setTimeout(function() {
+        // Ocultar la página 1
+        document.getElementById("page1").style.display = "none";
+        
+        // Cargar la página 2 usando AJAX
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Insertar el contenido de la página 2 en el DOM
+                document.getElementById("page-container").innerHTML = this.responseText;
+                
+                // Aplicar la animación de entrada a la página 2
+                document.getElementById(window.location.href = 'asignar_horario/').classList.add("fade-in");
+            }
+        };
+        xhttp.open("GET", window.location.href = 'asignar_horario/', true);
+        xhttp.send();
+    }, 500);
+}
+
+function redirigirConsultarHorario() {
+    document.getElementById("page1").classList.add("fade-out");
+    
+    setTimeout(function() {
+        // Ocultar la página 1
+        document.getElementById("page1").style.display = "none";
+        
+        // Cargar la página 2 usando AJAX
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Insertar el contenido de la página 2 en el DOM
+                document.getElementById("page-container").innerHTML = this.responseText;
+                
+                // Aplicar la animación de entrada a la página 2
+                document.getElementById(window.location.href = 'consultar_horarios/').classList.add("fade-in");
+            }
+        };
+        xhttp.open("GET", window.location.href = 'consultar_horarios/', true);
+        xhttp.send();
+    }, 500);
+}
+
+function redirigirServiciosAsignaciondesdeConsultarHorarios() {
+    document.getElementById("page1").classList.add("fade-out");
+    
+    setTimeout(function() {
+        // Ocultar la página 1
+        document.getElementById("page1").style.display = "none";
+        
+        // Cargar la página 2 usando AJAX
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Insertar el contenido de la página 2 en el DOM
+                document.getElementById("page-container").innerHTML = this.responseText;
+                
+                // Aplicar la animación de entrada a la página 2
+                document.getElementById(window.location.href = '/index/servicios_asignacion').classList.add("fade-in");
+            }
+        };
+        xhttp.open("GET", window.location.href = '/index/servicios_asignacion', true);
+        xhttp.send();
+    }, 500);
+}
+
+function redirigirModificarHorario() {
+    document.getElementById("page1").classList.add("fade-out");
+    
+    setTimeout(function() {
+        // Ocultar la página 1
+        document.getElementById("page1").style.display = "none";
+        
+        // Cargar la página 2 usando AJAX
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Insertar el contenido de la página 2 en el DOM
+                document.getElementById("page-container").innerHTML = this.responseText;
+                
+                // Aplicar la animación de entrada a la página 2
+                document.getElementById(window.location.href = 'modificar_horarios/').classList.add("fade-in");
+            }
+        };
+        xhttp.open("GET", window.location.href = 'modificar_horarios/', true);
+        xhttp.send();
+    }, 500);
+}
+
+function redirigirCrearEspacio() {
+    document.getElementById("page1").classList.add("fade-out");
+    
+    setTimeout(function() {
+        // Ocultar la página 1
+        document.getElementById("page1").style.display = "none";
+        
+        // Cargar la página 2 usando AJAX
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Insertar el contenido de la página 2 en el DOM
+                document.getElementById("page-container").innerHTML = this.responseText;
+                
+                // Aplicar la animación de entrada a la página 2
+                document.getElementById(window.location.href = 'crear_espacio/').classList.add("fade-in");
+            }
+        };
+        xhttp.open("GET", window.location.href = 'crear_espacio/', true);
+        xhttp.send();
+    }, 500);
+}
