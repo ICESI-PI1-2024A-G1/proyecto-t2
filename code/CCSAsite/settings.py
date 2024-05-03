5e58698512cc5b6b146028969c58f6b5c5f8264e
Juan Felipe Jojoa Crespo <juanfejojoa@gmail.com>
2024-05-02 22:03:43 -0500

    JFJC/fix: Fix the previous commit

--- code/CCSAsite/settings.py
+++ code/CCSAsite/settings.py
@@ -84,14 +84,19 @@ WSGI_APPLICATION = 'CCSAsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
