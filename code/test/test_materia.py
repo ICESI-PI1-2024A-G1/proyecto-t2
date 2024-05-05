2a531967928858b6ffcbb279843e2849161ba9c1
Samuel Alvarez Alban <samuelalvarez2221@gmail.com>
2024-05-05 17:06:22 -0500

    SAA/feat: login, edit, search test complete

--- code/test/test_materia.py
+++ code/test/test_materia.py
@@ -1,7 +1,6 @@
 import pytest
 from CCSApp.models import Materia, Departamento, Semestre
-from django.test import RequestFactory
-from CCSApp.views import buscar_materia
+from django.test import TestCase
 from CCSApp.forms import MateriaSearchForm
 @pytest.mark.django_db
 def test_materia_creation():
@@ -11,23 +10,20 @@ def test_materia_creation():
     assert materia.nombre_materia == 'Introducción a la Ingeniería de Software'
 
 
-@pytest.mark.django_db
-def test_buscar_materia_view():
-    # Creamos una materia para probar
-    materia = Materia.objects.create(nombre_materia='Matemáticas', codigo_materia='MAT101', creditos_materia=3)
+class TestMateriaSearchForm(TestCase):
+    def test_valid_form(self):
+        # Creamos datos válidos para el formulario
+        form_data = {'nombre_materia': 'Introducción a la Ingeniería de Software'}
+        form = MateriaSearchForm(data=form_data)
 
-    # Creamos un request ficticio con método POST y datos de formulario
-    request = RequestFactory().post('/buscar-materia/', {'nombre_materia': 'Mat'})
+        # Verificamos que el formulario sea válido
+        self.assertTrue(form.is_valid())
 
-    # Ejecutamos la vista
-    response = buscar_materia(request)
+    def test_invalid_form(self):
+        # Creamos datos inválidos para el formulario
+        form_data = {'nombre_materia': ''}
+        form = MateriaSearchForm(data=form_data)
 
-    # Verificamos que se haya renderizado correctamente la plantilla
-    assert response.status_code == 200
-
-    # Verificamos que el formulario esté presente en el contexto de la respuesta
-    assert 'form' in response.context
-
-    # Verificamos que la materia se haya filtrado correctamente
-    materias = response.context['materias']
-    assert materias.count() == 1
+        # Verificamos que el formulario sea inválido
+        self.assertFalse(form.is_valid())
+        
--- code/test/test_usuario.py
+++ code/test/test_usuario.py
@@ -1,10 +1,11 @@
 import pytest
-from django.test import RequestFactory
-from django.contrib.auth.models import User
-from CCSApp.models import Usuario
-from CCSApp.views import log_in
 
-from CCSApp.models import *
+from CCSApp.models import Usuario
+from django.test import TestCase, Client
+from CCSApp.forms import LoginForm
+from django.urls import reverse
+
+
 
 @pytest.mark.django_db
 def test_usuario_creation():
@@ -20,49 +21,23 @@ def test_usuario_creation():
     )
     assert usuario.cedula == '123456789'
     
-@pytest.mark.django_db   
-def test_log_in_view():
-    # Creamos un usuario para utilizarlo en las pruebas
-    user = Usuario.objects.create(
-        nombre='Usuario Prueba',
-        cedula='1234567890',
-        rol='admin',
-        departamento='TIC',
-        correo_electronico='usuario@example.com',
-        telefono=123456789,
-        password='password123'
-    )
+@pytest.mark.django_db
+class TestLoginForm(TestCase):
+    def test_valid_form(self):
+        # Creamos datos válidos para el formulario
+        form_data = {'cedula': '1234567890', 'password': 'password123'}
+        form = LoginForm(data=form_data)
 
-    # Creamos una solicitud HTTP POST con datos de formulario válidos
-    request = RequestFactory().post('', {
-        'cedula': '1234567890',
-        'password': 'password123'
-    })
+        # Verificamos que el formulario sea válido
+        self.assertTrue(form.is_valid())
 
-    # Agregamos el usuario autenticado en la solicitud
-    request.user = user
+    def test_invalid_form(self):
+        # Creamos datos inválidos para el formulario
+        form_data = {'cedula': '', 'password': ''}
+        form = LoginForm(data=form_data)
 
-    # Llamamos a la vista y obtenemos la respuesta
-    response = log_in(request)
+        # Verificamos que el formulario sea inválido
+        self.assertFalse(form.is_valid())
+        
 
-    # Verificamos que la respuesta sea una redirección
-    assert response.status_code == 302
 
-    # Verificamos que la redirección se haga a la página de inicio correcta
-    assert response.url == '/index/'
-
-    # También podemos probar un escenario donde las credenciales no son válidas
-    # Creamos una solicitud HTTP POST con datos de formulario inválidos
-    request_invalid = RequestFactory().post('', {
-        'cedula': '1234567890',
-        'password': 'claveincorrecta'
-    })
-
-    # Llamamos a la vista con las credenciales inválidas
-    response_invalid = log_in(request_invalid)
-
-    # Verificamos que la vista renderice la plantilla de inicio de sesión nuevamente
-    assert response_invalid.status_code == 200
-
-    # Verificamos que el mensaje de error sea visible en la respuesta renderizada
-    assert 'Usuario o clave incorrecta, intente de nuevo' in response_invalid.content.decode('utf-8')
