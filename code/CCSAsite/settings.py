05893efab7cb698ecb563b7c46ed9b855fd620c4
Juan Felipe Jojoa Crespo <juanfejojoa@gmail.com>
2024-05-05 12:45:43 -0500

    JFJC/feat: Deployment Test

--- code/CCSAsite/settings.py
+++ code/CCSAsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-# import dj_database_urlcl
+import dj_database_url
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'CCSAsite.wsgi.application'
 
 #Este bloque de codigo no puede ser eliminado, ya que es necesario para la conexion a la base de datos en el deployment
 #Solo dejarlo comentado a la hora de hacer cambios localmente
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://postgres:postgres@localhost:5432/mysite',
-#         conn_max_age=600
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
 
 #Este bloque de codigo no puede ser eliminado, ya que es necesario para hacer ediciones o trabajar localmente el proyecto
 #Solo dejarlo comentado a la hora de hacer deploy en Render
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
